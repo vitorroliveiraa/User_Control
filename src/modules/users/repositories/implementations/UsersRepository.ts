834bf73d01e1f3df75d38adb8e8c5cd472c066cf
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2021-08-30 10:42:36 -0300

    Criação de usuário ✔

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -2,41 +2,51 @@ import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
 
 class UsersRepository implements IUsersRepository {
-  private users: User[];
+    private users: User[];
 
-  private static INSTANCE: UsersRepository;
+    private static INSTANCE: UsersRepository;
 
-  private constructor() {
-    this.users = [];
-  }
-
-  public static getInstance(): UsersRepository {
-    if (!UsersRepository.INSTANCE) {
-      UsersRepository.INSTANCE = new UsersRepository();
+    private constructor() {
+        this.users = [];
     }
 
-    return UsersRepository.INSTANCE;
-  }
+    public static getInstance(): UsersRepository {
+        if (!UsersRepository.INSTANCE) {
+            UsersRepository.INSTANCE = new UsersRepository();
+        }
 
-  create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
-  }
+        return UsersRepository.INSTANCE;
+    }
 
-  findById(id: string): User | undefined {
-    // Complete aqui
-  }
+    create({ name, email }: ICreateUserDTO): User {
+        const user = new User();
 
-  findByEmail(email: string): User | undefined {
-    // Complete aqui
-  }
+        Object.assign(user, {
+            name,
+            email,
+            created_at: new Date(),
+        });
 
-  turnAdmin(receivedUser: User): User {
-    // Complete aqui
-  }
+        this.users.push(user);
 
-  list(): User[] {
-    // Complete aqui
-  }
+        return user;
+    }
+
+    findById(id: string): User | undefined {
+        // Complete aqui
+    }
+
+    findByEmail(email: string): User | undefined {
+        // Complete aqui
+    }
+
+    turnAdmin(receivedUser: User): User {
+        // Complete aqui
+    }
+
+    list(): User[] {
+        // Complete aqui
+    }
 }
 
 export { UsersRepository };
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,18 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ name, email }: IRequest): User {
+        const user = this.usersRepository.create({ name, email });
+
+        return user;
+    }
 }
 
 export { CreateUserUseCase };
